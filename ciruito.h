f9a53d84eaeec8a5c45803a47abf28050d9d88d0
Jamile Ferino <82777143+Jamile21@users.noreply.github.com>
2021-06-20 21:30:33 +0000

    Atualização1

--- ciruito.h
+++ ciruito.h
@@ -6,21 +6,38 @@ namespace crt{
   float R1,R2;
   float Rs, Rp;
   float V,I,P;
+  int R;
 
   cout << "Analise de Circuito Serie-Paralelo" << endl;
   cout<<"Insira o valor da fonte (volts):"<<endl;
   cin>> V;
-  cout << "Insira os valores das resistencias (ohms)\nR1:\t";
+    cout << "Insira os valores das resistencias (ohms)\nR1:\t";
   cin >> R1;
   cout << "R2:\t";
   cin >> R2;
+  cout<< "Os resistores estão em série ou paralelo?"<<endl;
+  cout<< "Digite 1 para série ou 2 para paralelo"<<endl;
+  cin>>R;
+  
+  if(R==1){
 
-  Rs=R1+R2;
-  Rp=(R1*R2)/(Rs);
-  I=V/Rs;
-  P=V/I;
-cout<<"A resistencia em serie é:"<<Rs<<endl;
-cout<<"A resistencia em pararelo é:"<<Rp<<endl;
+    Rs=R1+R2;
+    cout<<"A resistencia em serie é:"<<Rs<<"ohms"<<endl;
+    cout<<"A corrente total é:" << I= V/Rs << "A"<< endl;
+    cout<<"A potencia total é:" << P=V/I << "W"<< endl;
+  }
+  if(R==2)
+  {
+
+    Rp=((R1*R2)/(R1+R2));
+    cout<<"A resistencia em pararelo é:"<< Rp << "ohms"<<endl;
+    cout<<"A corrente total é:" << I= V/Rp << "A"<< endl;
+    cout<<"A potencia total é:" << P=V/I << "W"<< endl;
+  }
+ 
+  
+
+
 
   }
 }
